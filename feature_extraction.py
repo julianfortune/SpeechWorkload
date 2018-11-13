d9a620791b12e314a33d0aaa661da2e51195345d
Julian Fortune <juliandfortune@gmail.com>
2018-11-12 17:30:31 -0800

    Tidied up and ready to produce feature arrays

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,24 +1,24 @@
-import os, numpy, scipy, time, io
-import scipy.signal # used to extract power spectrum
-from scipy.io.wavfile import read, write #writes temp file of window for sr to use
-import wavio # converts wav to numpy array
-import speech_recognition as sr # neede for sphync transcript
-import matplotlib.pyplot as plt # needed for visualisation
+import os, numpy, scipy, time, io, glob
+import scipy.signal # Extracts power spectrum
+from scipy.io.wavfile import read, write # Writes temp file of window for sr to use
+import wavio # Converts wav to numpy array
+import speech_recognition as sr # Sphynx transcript
+import matplotlib.pyplot as plt # Visualisation
 
 numpy.set_printoptions(threshold=numpy.inf)
 
 # | Gets transcript of audio converted to text by Sphinx, returns array of
-# | words (strings). Writes "<fileName>-temp.wav" to work with sr; deletes before exiting
+# | words (as strings). Writes "<fileName>-temp.wav" to work with sr; deletes before exiting
+# | Returns:
+# |   Python array of words detected in audio
 def getTranscriptFromData(data,sampleRate,filePath):
-
     tempFilePath = filePath + "-temp.wav"
-
     wavio.write(tempFilePath, data, sampleRate, sampwidth=1)
 
-    r = sr.Recognizer() # initialize recognizer object
+    r = sr.Recognizer() # Initialize recognizer object
     r.energy_threshold = 200;
     with sr.AudioFile(tempFilePath) as source:
-        audio = r.record(source)  # reads in the entire audio file
+        audio = r.record(source)  # Reads in the entire audio file
     transcript = []
     try:
         transcript += r.recognize_sphinx(audio).split()
@@ -27,27 +27,24 @@ def getTranscriptFromData(data,sampleRate,filePath):
 
     os.remove(tempFilePath)
 
-    return transcript #returns python array of words detected in audio
+    return transcript
 
 # | Gets power of sound and returns numpy arrays
 def getPowerSpectrum(data,sampleRate,windowSize):
     freqs,ps = scipy.signal.welch(data,
                                   sampleRate,
-                                  window='hanning',   # apply a Hanning window
-                                  nperseg= int(windowSize), # compute periodograms of ____-long segments of x
+                                  window='hanning',   # Apply a Hanning window
+                                  nperseg= int(windowSize), # Compute periodograms of ____-long segments of x
                                   )
     return freqs,ps
 
 # | Compares power of each window to THRESHOLD and returns numpy array with
 # | 1s representing speech and 0s representing silence
-def getVoiceActivity(data,sampleRate,windowSize,thresholdForSpeech):
-    minFreq = 300 # in hertz the lowest frequency of the voice band
-    maxFreq = 3000 # in hertz the highest frequency of the voice band
+def getVoiceActivity(data,sampleRate,thresholdForSpeech):
+    minFreq = 300 # In hertz the lowest frequency of the voice band
+    maxFreq = 3000 # In hertz the highest frequency of the voice band
 
-    if len(data) < windowSize:
-        windowSize = len(data)
-
-    freqs, ps = getPowerSpectrum(data,sampleRate,windowSize)
+    freqs, ps = getPowerSpectrum(data,sampleRate,len(data))
 
     if numpy.mean(ps[minFreq:maxFreq]) > thresholdForSpeech:
         return 1
@@ -56,10 +53,21 @@ def getVoiceActivity(data,sampleRate,windowSize,thresholdForSpeech):
     return 0
 
 # | Returns the average voice activity (0 <= v <= 1)
-def getAverageVoiceActivity():
-    voiceActivity = numpy.empty
+def getVoiceActivityFeatures(data,sampleRate,windowSize,thresholdForSpeech):
+    sampleWindowSize = windowSize * sampleRate
+    voiceActivity = numpy.zeros(shape=0)
 
-    numpy.append(voiceActivity,getVoiceActivity(chunk,sampleRate,sampleStepSize,threshold))
+    step = 0
+    while step < len(data):
+        voiceActivity = numpy.append(voiceActivity,getVoiceActivity(data[step:step + sampleWindowSize],sampleRate,thresholdForSpeech))
+
+        # Increment to next step
+        step += sampleWindowSize
+
+    averageVoiceActivity = numpy.mean(voiceActivity)
+    voiceActivityStDev = numpy.std(voiceActivity)
+    return averageVoiceActivity, voiceActivityStDev
+
 
 # | Goes over whole audiofile and finds loudest noise in band of vocal activity,
 # | multiplies by the supplied percentage and returns. Any noise above this
@@ -72,11 +80,11 @@ def getVADThreshold(data,sampleRate,minFreq,maxFreq,percentageThreshold,windowSi
     for window in range(0,int(len(data) / sampleWindowSize)):
         windowStart = int(window * sampleWindowSize)
         windowEnd = int(windowStart + sampleWindowSize)
-        if windowEnd < len(data): #catch running off the end of the wav array
+        if windowEnd < len(data): # Catch running off the end of the wav array
             freqs,ps = scipy.signal.welch(data[windowStart:windowEnd],
                                           sampleRate,
-                                          window='hanning',   # apply a Hanning window
-                                          nperseg= int(sampleWindowSize), # compute periodograms of ____-long segments of x
+                                          window='hanning',   # Apply a Hanning window
+                                          nperseg= int(sampleWindowSize), # Compute periodograms of ____-long segments of x
                                           )
 
             rms = numpy.append(rms, numpy.mean(ps[minFreq:maxFreq]))
@@ -88,29 +96,30 @@ def getVADThreshold(data,sampleRate,minFreq,maxFreq,percentageThreshold,windowSi
 # | Computes the absolute value of the raw data values then calculates
 # | the mean, max, min, and standard deviation of those data values
 def getIntensityFeatures(data):
-    abs_val_data = numpy.absolute(data)
-    average_intensity = numpy.mean(abs_val_data)
-    max = numpy.amax(abs_val_data)
-    min = numpy.amin(abs_val_data)
-    stDev = numpy.std(abs_val_data)
-    return average_intensity, stDev, max, min
+    absVal = numpy.absolute(data)
+    average = numpy.mean(absVal)
+    max = numpy.amax(absVal)
+    min = numpy.amin(absVal)
+    stDev = numpy.std(absVal)
+    return average, stDev
 
 # | Computes welch looking back and for number of sampleRate in length of data
 # | and returns the average of the loudest pitch in each window
-def getAveragePitch(data,sampleRate,windowSize):
-    sampleWindowSize = windowSize * sampleRate #windowSize in seconds
-    loudest_pitch = numpy.zeros(shape=0)
+def getPitchFeatures(data,sampleRate,windowSize):
+    sampleWindowSize = windowSize * sampleRate # windowSize in seconds
+    loudestPitch = numpy.zeros(shape=0)
 
     step = 0
     while step < len(data):
         freqs, ps = getPowerSpectrum(data[step:step + sampleWindowSize],sampleRate,sampleRate)
-        loudest_pitch = numpy.append(loudest_pitch, numpy.argmax(ps))
+        loudestPitch = numpy.append(loudestPitch, numpy.argmax(ps))
 
-        #increment to next step
+        # Increment to next step
         step += sampleWindowSize
-    average_pitch = numpy.mean(loudest_pitch)
+    average = numpy.mean(loudestPitch)
+    stDev = numpy.std(loudestPitch)
 
-    return average_pitch
+    return average, stDev
 
 # | Extracts all features and returns array in accordance with Jamison's drawing
 # | Parameters:
@@ -119,6 +128,8 @@ def getAveragePitch(data,sampleRate,windowSize):
 # |   - lookBackSize: size of window for looking back on longterm features in seconds
 # |   - percentageThreshold: percentage of loudest noise in band above which vocal activity is present
 # |   - printStatus: if True will print out as each second is processed, else no print
+# | Returns:
+# |   - Numpy array with features
 def getFeatures(filePath,stepSize,lookBackSize,percentageThreshold,printStatus):
     if printStatus :
         print("[ START ] Working on:",filePath)
@@ -130,28 +141,29 @@ def getFeatures(filePath,stepSize,lookBackSize,percentageThreshold,printStatus):
     width = audioData.sampwidth # bit depth is equal to width times 8
     length = len(audioData.data) # gets number of sample in audio
 
-    # make the audio data mono
+    # Make the audio data mono
     data = numpy.mean(audioData.data,axis=1)
 
-    #set up arrays for features
+    # Set up arrays for features
     seconds = numpy.zeros(shape=0)
-    voiceActivity = numpy.zeros(shape=0)
     wpm = numpy.zeros(shape=0)
-    pitch = numpy.zeros(shape=0)
+    meanVoiceActivity = numpy.zeros(shape=0)
+    stDevVoiceActivity = numpy.zeros(shape=0)
+    meanPitch = numpy.zeros(shape=0)
+    stDevPitch = numpy.zeros(shape=0)
     meanIntensity = numpy.zeros(shape=0)
     stDevIntensity = numpy.zeros(shape=0)
-    maxIntensity = numpy.zeros(shape=0)
-    minIntensity = numpy.zeros(shape=0) # this will just be all zeroes
 
     if printStatus :
         print("Determining threshold for speech...")
 
-    threshold = getVADThreshold(data,           #audio data
+    threshold = getVADThreshold(data,           # audio data
                                 sampleRate=sampleRate,
-                                minFreq=300,    #minimum frequency of vocal activity
-                                maxFreq=3000,   #max frequency of vocal activity
+                                minFreq=300,    # minimum frequency of vocal activity
+                                maxFreq=3000,   # max frequency of vocal activity
                                 percentageThreshold=percentageThreshold
                                 )
+
     if printStatus :
         print("Done!")
 
@@ -159,11 +171,8 @@ def getFeatures(filePath,stepSize,lookBackSize,percentageThreshold,printStatus):
     sampleStepSize = int(stepSize * sampleRate)
     sampleLookBackSize = int(lookBackSize * sampleRate)
     while step < length:
-        if printStatus :
+        if printStatus:
             print("[",str(step/length*100)[:4],"% ] Second",int(step/sampleRate))
-
-        # cut out the chunk to look at
-        chunk = data[step:step + sampleStepSize]
 
         # keep track of what second we're in
         seconds = numpy.append(seconds,step/sampleRate)
@@ -173,38 +182,41 @@ def getFeatures(filePath,stepSize,lookBackSize,percentageThreshold,printStatus):
 
             lookBackChunk = data[step + sampleStepSize - sampleLookBackSize:step + sampleStepSize]
 
-            ### VAD
-
-
             ### WORDS PER MINUTE
             transcript = getTranscriptFromData(lookBackChunk,sampleRate,filePath)
-            current_wpm = len(transcript)/( lookBackSize/60 )
-            wpm = numpy.append(wpm,current_wpm)
+            currentWPM = len(transcript)/( lookBackSize/60 )
+            wpm = numpy.append(wpm,currentWPM)
+
+            ### VAD
+            average, stDev = getVoiceActivityFeatures(lookBackChunk,sampleRate,1,threshold)
+            meanVoiceActivity = numpy.append(meanVoiceActivity,average)
+            stDevVoiceActivity = numpy.append(stDevVoiceActivity,stDev)
 
             ### AVERAGE PITCH
-            pitch = numpy.append(pitch, getAveragePitch(lookBackChunk,sampleRate,1))
+            average, stDev = getPitchFeatures(lookBackChunk,sampleRate,1)
+            meanPitch = numpy.append(meanPitch, average)
+            stDevPitch = numpy.append(stDevPitch, stDev)
 
-            ## INTENSITY FEATURES
-            average_intensity, stDev, max, min = getIntensityFeatures(lookBackChunk)
-            meanIntensity = numpy.append(meanIntensity, average_intensity)
+            ### INTENSITY FEATURES
+            average, stDev = getIntensityFeatures(lookBackChunk)
+            meanIntensity = numpy.append(meanIntensity, average)
             stDevIntensity = numpy.append(stDevIntensity, stDev)
-            maxIntensity = numpy.append(maxIntensity, max)
-            minIntensity = numpy.append(minIntensity, min)
 
-        # fills arrays with zeros until step is larger than lookBackSize
+        # Fills arrays with zeros until step is larger than lookBackSize
         else:
-
-            wpm = numpy.append(wpm,0)
-            pitch = numpy.append(pitch,0)
+            wpm = numpy.append(wpm, 0)
+            meanVoiceActivity = numpy.append(meanVoiceActivity, 0)
+            stDevVoiceActivity = numpy.append(stDevVoiceActivity, 0)
+            meanPitch = numpy.append(meanPitch, 0)
+            stDevPitch = numpy.append(stDevPitch, 0)
             meanIntensity = numpy.append(meanIntensity, 0)
             stDevIntensity = numpy.append(stDevIntensity, 0)
-            maxIntensity = numpy.append(maxIntensity, 0)
-            minIntensity = numpy.append(minIntensity, 0)
 
-        # increment to next step
+        # Increment to next step
         step += sampleStepSize
 
-    features = numpy.vstack([seconds,voiceActivity,wpm,pitch,meanIntensity,stDevIntensity,maxIntensity,minIntensity])
+    # Pulls all the feautures together in one array
+    features = numpy.vstack([seconds,wpm,meanVoiceActivity,stDevVoiceActivity,meanPitch,stDevPitch,meanIntensity,stDevIntensity])
 
     if printStatus :
         print("[ DONE ] Finished processing",filePath,"!")
@@ -212,15 +224,32 @@ def getFeatures(filePath,stepSize,lookBackSize,percentageThreshold,printStatus):
     return features
 
 def main():
-    path1 = "./media/pilot/pilot_1_ol.wav"
-    path2 = "./media/Speech16.wav"
-    featureArray = getFeatures(filePath=path2, #file
-                               stepSize=1, #how big to step
-                               lookBackSize=30,  #how big of interval to wait until looking for transcript, pitch/intensity features
-                               percentageThreshold=0.001,  #percentage of loudest noise in band above which vocal activity is present
-                               printStatus=True
-                               )
-    print(featureArray)
+    dir = "../media/Participant_Audio/*.wav"
+
+    # Keep track of running stats
+    startTime = time.time()
+    count = 1
+
+    for path in sorted(glob.iglob(dir)):
+        # Communicate progress
+        print("[ " + str(count) + "/" + str(len(sorted(glob.iglob(dir)))) + " ] \tStarting:",path)
+
+        featureArray = getFeatures(filePath=path, #file
+                                   stepSize=1, #how big to step in seconds
+                                   lookBackSize=30,  #how big of interval to wait until looking for transcript, pitch/intensity features in seconds
+                                   percentageThreshold=0.001,  #percentage of loudest noise in band above which vocal activity is present
+                                   printStatus=False
+                                   )
+
+        # Save the numpy array
+        numpy.save("./features/" + os.path.basename(path)[:-4],featureArray)
+
+        # Crunch some numbers and communicate to the user
+        timeElapsed = time.time() - startTime
+        estimatedTimeRemaining = timeElapsed/count * (len(sorted(glob.iglob(dir))) - count)
+        print("\t\t" + str(timeElapsed/60) + " minutes elapsed. Estimated time remaining: " + str(estimatedTimeRemaining/60))
+
+        count += 1
 
 if __name__ == "__main__":
     main()
