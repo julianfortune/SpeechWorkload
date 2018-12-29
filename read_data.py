be2442d1df08690fcd1e02f186f164a3509576cc
Julian Fortune <juliandfortune@gmail.com>
2018-12-29 10:27:59 -0800

    Adds real data and parsing

---
+++ read_data.py
@@ -0,0 +1,41 @@
+'''
+Created on Jul 10, 2017
+
+@author: Jamison Heard
+'''
+
+import tflearn.data_utils as data
+from tflearn.data_utils import load_csv
+import numpy as np
+
+def import_file(file):
+    data,label = load_csv(file, has_header = True)
+    return data
+
+def parse_time(time_string):
+    minute_string = time_string[0:2]
+    second_string = time_string[3:]
+    seconds = int(minute_string) * 60 + int(round(float(second_string), 0))
+    return seconds
+
+
+def main():
+    file_name = "./actual/normal_load.csv"
+    data = import_file(file_name)
+    output = []
+    current_level = 0
+    for index in range(0,len(data)):
+        row = data[index]
+        current_time = parse_time(row[0])
+        current_level = int(row[10])
+        output.append(current_level)
+        if index + 1 < len(data):
+            next_time = parse_time(data[index + 1][0])
+            for interpolated_time in range(current_time + 1, next_time):
+                output.append(current_level)
+
+    for row in output:
+        print(row)
+
+if __name__ == "__main__":
+    main()
