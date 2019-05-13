bd43467e95ca19f164d7843ef78609ee134d53b0
Julian Fortune <juliandfortune@gmail.com>
2019-05-12 20:03:19 -0700

    Removing Duplicates

--- testingOffline.py
+++
@@ -1,8 +0,0 @@
-from speechLibrary import speechAnalysis
-from speechLibrary import audioModule
-import numpy
-
-analyzer = speechAnalysis.SpeechAnalyzer()
-analyzer.printStatus = True
-featureArray = analyzer.getFeaturesFromFile("../media/Participant_Audio/p1_ol.wav")
-numpy.save("./test_features/p1_ol", featureArray)
