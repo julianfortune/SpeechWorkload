7bb29c578ab279534cef8f920fcd1d97557fc916
Julian Fortune <juliandfortune@gmail.com>
2019-05-22 12:29:22 -0700

    Fixes feature comparing

--- speechLibrary/testModule.py
+++ speechLibrary/testModule.py
@@ -6,7 +6,7 @@ yellow = "\u001b[33m"
 blue = "\u001b[34m"
 reset = "\u001b[0m"
 
-featureNames = ["time", "voiceActivity", "filledPauses"]
+featureNames = ["time", "syllablesPerSecond", "meanVoiceActivity", "stDevVoiceActivity", "meanPitch", "stDevPitch", "meanIntensity", "stDevIntensity"]
 
 def compareArrays(old, new):
     print()
@@ -14,12 +14,12 @@ def compareArrays(old, new):
     if np.array_equal(old, new):
         print(green + "  No changes!" + reset)
     else:
-        numberOfColumnsInOld = oldFeatures.shape[1]
-        numberOfColumnsInNew = newFeatures.shape[1]
+        numberOfColumnsInOld = old.shape[0]
+        numberOfColumnsInNew = new.shape[0]
 
         for columnIndex in range(numberOfColumnsInOld):
-            oldColumn = old[:,columnIndex]
-            newColumn = new[:,columnIndex]
+            oldColumn = old[columnIndex]
+            newColumn = new[columnIndex]
 
             if np.array_equal(oldColumn, newColumn):
                 print(green + "  '" + featureNames[columnIndex] + "' unchanged" + reset)
@@ -38,8 +38,8 @@ def compareArrays(old, new):
                 str(max(differences)) + reset)
     print()
 
-def compareArrayFiles(oldFeaturesFile, newFeatures):
-    oldFeatures = numpy.load(oldFeaturesPath)
-    newFeatures = numpy.load(newFeaturesPath)
+def compareArrayFiles(oldFeaturesPath, newFeaturesPath):
+    oldFeatures = np.load(oldFeaturesPath)
+    newFeatures = np.load(newFeaturesPath)
 
     compareArrays(oldFeatures, newFeatures)
