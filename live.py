7f51c027ea2dc862facfba176f3c69ec3719c1b4
Julian Fortune <fortunej@oregonstate.edu>
2019-08-07 10:46:59 -0700

    Path tweaks

--- live.py
+++ live.py
@@ -1,10 +1,8 @@
 from speechLibrary import speechAnalysis
 
 analyzer = speechAnalysis.SpeechAnalyzer()
-
 analyzer.lookBackSize = 5
 analyzer.stepSize = 1
-
 analyzer.recordingBufferSize = 4096
 
 analyzer.getFeaturesFromLiveInput()
--- offline.py
+++ offline.py
@@ -2,11 +2,11 @@ from speechLibrary import speechAnalysis
 from speechLibrary import audioModule
 import numpy
 
+audioDirectory = "../media/Jamison_Evaluation/Supervisory/Day1"
+outputDirectory = "./training/Supervisory_Evaluation_Day_1/features/current5second/"
+
 analyzer = speechAnalysis.SpeechAnalyzer()
-
-#Leave default parameters
-
 analyzer.printStatus = True
 analyzer.lookBackSize = 5
 
-analyzer.createFeatureFilesFromDirectory("../media/Participant_Audio/", "./features/current5second/")
+analyzer.createFeatureFilesFromDirectory(audioDirectory, outputDirectory)
