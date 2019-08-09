00ef1dfb349b8f33c61a12bf6445d71425809627
Julian Fortune <juliandfortune@gmail.com>
2019-08-09 16:37:44 -0700

    Training and analyses progress

--- speech_workload_training.py
+++ speech_workload_training.py
@@ -11,6 +11,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tflearn
 import pandas as pd
+from scipy import stats
 
 # np.set_printoptions(threshold=sys.maxsize)
 
@@ -100,26 +101,24 @@ def loadData(directory, trainingFiles=[], inputFeaturesToDiscard=[], filterWithV
 
     return dataFrame
 
-def trainNetwork(data, directory):
+def neuralNetwork(data, directory, train= True):
     inputs = data.drop(columns=['speechWorkload']).to_numpy()
     labels = np.reshape(data['speechWorkload'].to_numpy(), (-1, 1))
+
+    # Shuffle data
+    inputs, labels = tflearn.data_utils.shuffle(inputs, labels)
 
     # Neural network characteristics
     input_neurons = inputs.shape[1] # Size in the second dimension
     hidden_neurons = 256
     output_neurons = 1
 
-    n_epoch = 500 #try 20 - 2000
-
-    name = "SpeechWorkload"
+    n_epoch = 50 #try 20 - 2000
 
     with tf.device('/gpu:0'):
         # Set up
         tf.reset_default_graph()
         tflearn.init_graph()
-
-        # Shuffle data
-        inputs, labels = tflearn.data_utils.shuffle(inputs, labels)
 
         # Input layer
         net = tflearn.input_data(shape=[None, input_neurons])
@@ -138,8 +137,9 @@ def trainNetwork(data, directory):
         # Create the model from the network
         model = tflearn.DNN(net, tensorboard_verbose=0)
 
-        # Fit the data, `validation_set=` sets asside a proportion of the data to validate with
-        model.fit(inputs, labels, n_epoch=n_epoch, validation_set=0.10, show_metric=True)
+        if train:
+            # Fit the data, `validation_set=` sets asside a proportion of the data to validate with
+            model.fit(inputs, labels, n_epoch=n_epoch, validation_set=0.10, show_metric=True)
 
         return model
 
@@ -147,34 +147,63 @@ def assessModelAccuracy(model, data):
     predictions = model.predict(data.drop(columns=['speechWorkload']).to_numpy())[:,0]
     predictions[data.meanVoiceActivity < 0.1] = 0
 
+    actual = data.speechWorkload.to_numpy()
+
     results = pd.DataFrame()
     results['predictions'] = predictions
     results['actual'] = data.speechWorkload
 
-    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-        print(results)
+    # graphData = pd.concat([results, data.drop(columns=['speechWorkload'])], axis=1)
 
-    # plt.plot(list(range(0, len(data.speechWorkload))), data.speechWorkload, list(range(0, len(predictions))), predictions)
-    results.plot()
-    plt.show()
+    # with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+    #     print(results)
+
+    correlationCoefficient = stats.pearsonr(actual, predictions)[0]
+
+    rmse = np.sqrt(np.mean((predictions - actual)**2, axis=0, keepdims=True))[0]
+
+    actualMean = np.mean(actual)
+    actualStandardDeviation = np.std(actual)
+    predictionsMean = np.mean(predictions)
+    predictionsStandardDeviation = np.std(predictions)
+
+    print(correlationCoefficient, rmse, actualMean, actualStandardDeviation, predictionsMean, predictionsStandardDeviation)
+
+    # results.plot()
+    # plt.show()
 
 def supervisoryLeaveOneOutCrossValidation():
     directory = "./training/Supervisory_Evaluation_Day_1/"
 
-    results = []
+    results = pd.DataFrame(columns=["file"])
 
-    for path in sorted(glob.iglob(directory + "features/*.npy")):
+    trainModelsAndSave = True
+    participants = []
+
+    for participantNumber in range(1, 31):
+        print("Participant", participantNumber)
+
+        participantPaths = []
+
+        for path in sorted(glob.iglob(directory + "features/*.npy")):
+
+            if str(participantNumber) == os.path.basename(path).split('_')[0][1:]:
+                participantPaths.append(path)
 
         featurePaths = sorted(glob.iglob(directory + "features/*.npy"))
-        featurePaths.remove(path)
+
+        for path in participantPaths:
+            featurePaths.remove(path)
 
         train = loadData(directory, trainingFiles=featurePaths)
-        # test = loadData(directory, trainingFiles=path, filterWithVoiceActivity=False)
+        test = loadData(directory, trainingFiles=participantPaths, filterWithVoiceActivity=False)
 
-        model = trainNetwork(train, directory)
-        model.save(directory + "models/leaveOut-" + os.path.basename(path) + ".tflearn")
-
-        # metrics = assessModelAccuracy(model, test)
+        if trainModelsAndSave:
+            model = neuralNetwork(train, directory)
+            model.save(directory + "models/leaveOut-" + os.path.basename(path) + ".tflearn")
+        else:
+            model.load(directory + "models/leaveOut-" + os.path.basename(path) + ".tflearn")
+            metrics = assessModelAccuracy(model, test)
 
 def main():
     supervisoryLeaveOneOutCrossValidation()
