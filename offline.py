32599b37de543e89ca10136a20bc52f2aad9f05a
Julian Fortune <fortunej@oregonstate.edu>
2019-08-13 14:52:13 -0700

    Adds functions to offline processing for each evaluation.

--- offline.py
+++ offline.py
@@ -2,11 +2,27 @@ from speechLibrary import speechAnalysis
 from speechLibrary import audioModule
 import numpy
 
-audioDirectory = "../media/Jamison_Evaluations/Supervisory/Day1/"
-outputDirectory = "./training/Supervisory_Evaluation_Day_1/features/"
+def createFeatures(audioDirectory, outputDirectory):
+    analyzer = speechAnalysis.SpeechAnalyzer()
+    analyzer.printStatus = True
+    analyzer.lookBackSize = 5
 
-analyzer = speechAnalysis.SpeechAnalyzer()
-analyzer.printStatus = True
-analyzer.lookBackSize = 5
+    analyzer.createFeatureFilesFromDirectory(audioDirectory, outputDirectory)
 
-analyzer.createFeatureFilesFromDirectory(audioDirectory, outputDirectory)
+def day1():
+    createFeatures(audioDirectory= "../media/Jamison_Evaluations/Supervisory/Day1/",
+                   outputDirectory= "./training/Supervisory_Evaluation_Day_1/features/")
+
+def peerBased():
+    createFeatures(audioDirectory= "../media/Jamison_Evaluations/Peer_Based/Processed_Audio/",
+                   outputDirectory= "./training/Peer_Based/features/")
+
+def realTime():
+    createFeatures(audioDirectory= "../media/Jamison_Evaluations/Real_Time_Evaluation/Audio/",
+                   outputDirectory= "./training/Real_Time/features/")
+
+def main():
+    peerBased()
+    realTime()
+
+main()
